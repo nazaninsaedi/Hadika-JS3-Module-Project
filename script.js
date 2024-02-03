7ff3f4c99ce6941ef2dfa9e87ee507ff66c8307a
Your Name <you@example.com>
2024-02-03 16:35:00 +0000

    completed code

--- script.js
+++ script.js
@@ -6,7 +6,20 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  episodeList.map(({ name, season, number, image, summary }, index) => {
+    const card = document.getElementById("film-card").content.cloneNode(true);
+    card.querySelector("h3").textContent = name;
+    card.querySelector("#season", "#number").textContent = `S${padNumbers(season)}E${padNumbers(number)}`;
+    card.querySelector("#medium-img").src = image.medium;
+    card.querySelector("#summary").innerHTML = summary;
+    rootElem.append(card);
+    // console.log("in map", image);
+  })
+}
+
+function padNumbers(num) {
+  return String(num).padStart(2, '0');
 }
 
 window.onload = setup;
